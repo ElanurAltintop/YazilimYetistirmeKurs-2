bed1383ce40080f2f078140e3a422b10930f45b5
ElanurAltintop <77887554+ElanurAltintop@users.noreply.github.com>
2021-04-17 16:52:58 +0300

    Add files via upload
    C# programming languague camp-2

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace YazilimYetistirmeKurs_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+
+            //aray (dizi)
+            string[] kurslar = new string[]  //Tipi artık string array oldu
+            {
+                "Yazılım geliştirici yetiştirme kampı", "Programlamaya başlangıç için temel kurs",
+                "C# ve Java"
+            };
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)  //Dizileri tek tek dolaşmaya yarar
+            {
+                Console.WriteLine(kurs);
+            }  
+        }
+    }
+}
